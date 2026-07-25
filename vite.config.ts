0c9e5e606e8f6e41dbfe656f6e9c91019c2600ec
mxr7d <mohamedmurad035@gmail.com>
2026-07-25 20:22:29 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Tells Vite where your source files live relative to the repository root
+  root: './', 
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
